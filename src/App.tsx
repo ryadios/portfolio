842c8b2b6eb758d0049a8afe49f4d95194fe6dd0
Aditya Chaudhary <aditya.chaudhary0523@gmail.com>
2025-10-26 17:43:23 +0530

    feat: init base react-grid-layout files

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Layout from "@/components/layout";
+import Navbar from "@/components/navbar";
+
+export enum TabKey {
+    Home = "Home",
+    Work = "Work",
+    Blog = "Blog",
+    Contact = "Contact",
+}
+
+function App() {
+    const [tab, setTab] = useState<TabKey>(TabKey.Home);
+    const [x, setX] = useState(0);
+    const [w, setW] = useState(0);
+
+    return (
+        <main className="bg-[#f7f2f2]">
+            <Navbar
+                tab={tab}
+                setTab={setTab}
+                left={x}
+                sliderWidth={w}
+                setX={setX}
+                setW={setW}
+            />
+            <Layout tab={tab} setTab={setTab} left={x} sliderWidth={w} />
+        </main>
+    );
+}
+
+export default App;
---
+++ src/components/layout.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useMemo, useState } from "react";
+import { Responsive, WidthProvider } from "react-grid-layout";
+import { TabKey } from "../App";
+import {
+    ContactLayouts,
+    HomeLayouts,
+    NotesLayouts,
+    WorkLayouts,
+    keys,
+} from "@/utils/layout.helper";
+import { Card, CardHeader, CardTitle } from "./ui/card";
+
+interface LayoutProps {
+    tab: TabKey;
+    setTab: React.Dispatch<React.SetStateAction<TabKey>>;
+    left?: number;
+    sliderWidth?: number;
+}
+
+function Layout({ tab }: LayoutProps) {
+    const [currentlayout, setCurrentLayout] = useState(HomeLayouts);
+
+    useEffect(() => {
+        switch (tab) {
+            case TabKey.Work:
+                setCurrentLayout(WorkLayouts);
+                break;
+            case TabKey.Home:
+                setCurrentLayout(HomeLayouts);
+                break;
+            case TabKey.Contact:
+                setCurrentLayout(ContactLayouts);
+                break;
+            case TabKey.Blog:
+                setCurrentLayout(NotesLayouts);
+                break;
+            default:
+                setCurrentLayout(HomeLayouts);
+        }
+    }, [tab]);
+
+    const ResponsiveReactGridLayout = useMemo(
+        () => WidthProvider(Responsive),
+        []
+    );
+
+    return (
+        <div className="w-screen m-auto flex justify-between">
+            <ResponsiveReactGridLayout
+                className="m-auto w-[1200px] gap-12"
+                breakpoints={{ xl: 1200, lg: 899, md: 768, sm: 480, xs: 200 }}
+                cols={{ xl: 4, lg: 4, md: 2, sm: 1, xs: 1 }}
+                rowHeight={300}
+                layouts={currentlayout}
+                isResizable={false}
+            >
+                {keys.map((key) => (
+                    <Card
+                        key={key}
+                        className="flex justify-center items-center visible cursor-grab active:cursor-grabbing"
+                    >
+                        <CardHeader>
+                            <CardTitle>{key}</CardTitle>
+                        </CardHeader>
+                    </Card>
+                ))}
+            </ResponsiveReactGridLayout>
+        </div>
+    );
+}
+
+export default Layout;
