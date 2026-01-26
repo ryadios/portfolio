953fa26e0e510e11ce87c16d4893937c3899ffdd
Aditya Chaudhary <aditya.chaudhary0523@gmail.com>
2026-01-26 14:46:04 +0530

    chore: disable caching for page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ export default async function Home() {
             : "http://localhost:3000");
 
     try {
-        const res = await fetch(`${baseUrl}/api/spotify`);
+        const res = await fetch(`${baseUrl}/api/spotify`, {
+            cache: "no-store"
+        });
         if (!res.ok) throw new Error("Failed to fetch Spotify data");
         const data = await res.json();
 
