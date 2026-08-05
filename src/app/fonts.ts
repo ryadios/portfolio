7043a972f593c37f0be63cf48b2911131ffa4d19
Aditya Chaudhary <aditya.chaudhary0523@gmail.com>
2026-08-06 02:36:05 +0530

    perf: trim initial assets and dependencies

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -3,21 +3,6 @@ import localFont from "next/font/local";
 export const moranga = localFont({
     src: [
         {
-            path: "../../public/fonts/Moranga/Moranga-Light.otf",
-            weight: "300",
-            style: "normal",
-        },
-        {
-            path: "../../public/fonts/Moranga/Moranga-Regular.otf",
-            weight: "400",
-            style: "normal",
-        },
-        {
-            path: "../../public/fonts/Moranga/Moranga-Medium.otf",
-            weight: "500",
-            style: "normal",
-        },
-        {
             path: "../../public/fonts/Moranga/Moranga-Bold.otf",
             weight: "700",
             style: "normal",
@@ -25,6 +10,7 @@ export const moranga = localFont({
     ],
     variable: "--font-moranga",
     display: "swap",
+    preload: false,
 });
 
 export const silka = localFont({
@@ -37,11 +23,6 @@ export const silka = localFont({
         {
             path: "../../public/fonts/Silka/Silka-Medium.otf",
             weight: "500",
-            style: "normal",
-        },
-        {
-            path: "../../public/fonts/Silka/Silka-Bold.otf",
-            weight: "700",
             style: "normal",
         },
     ],
